2cad6a6239309f1ab9cc4a93341fb3f585877c3b
zearkiatos <caprilespe@outlook.com>
2021-07-12 20:24:14 -0400

    Interpolation, conditions and cycles

--- main.tf
+++ main.tf
@@ -7,3 +7,27 @@ resource "aws_instance" "demo-instance" {
   instance_type = var.instance_type
   tags          = var.tags
 }
+
+resource "aws_security_group" "ssh_connection" {
+  name        = var.sg_name
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "-1"
+    cidr_blocks      = # add a CIDR block here
+  }
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port        = ingress.value.from_port
+      to_port          = ingress.value.to_port
+      protocol         = ingress.value.protocol
+      cidr_blocks      = ingress.value.cidr_blocks
+    }
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
---
+++ prod.auto.tfvars
@@ -0,0 +1,6 @@
+ami_id        = "ami-08ee2516c7709ea48"
+instance_type = "t2.large"
+tags = {
+  Name        = "demo-1"
+  Environment = "production"
+}
