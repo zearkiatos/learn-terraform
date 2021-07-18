0ba8b5719e689f56ecc3f6f5f620302d757596f7
zearkiatos <caprilespe@outlook.com>
2021-07-17 20:52:37 -0400

    Local modules

---
+++ app.tf
@@ -0,0 +1,8 @@
+module "app-demo" {
+  source        = "./modules/instance"
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  tags          = var.tags
+  sg_name       = var.sg_name
+  ingress_rules = var.ingress_rules
+}
